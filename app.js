48dc58c3a8a9f76dce5f4e674a22cf76bbcc941b
mathiasburger <mburakyilmazer38@gmail.com>
2023-11-10 18:17:20 +0300

    expense

---
+++ app.js
@@ -0,0 +1,10 @@
+const formGelir = document.querySelector(".income-form")
+console.log(formGelir)
+const incomeValue = document.querySelector("#income")
+
+formGelir.addEventListener("submit", (e)=>{
+    e.preventDefault()
+console.log(incomeValue.value)
+
+
+})
